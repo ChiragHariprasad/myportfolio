88b3ed7466630b624b74e111549171e04b862c8d
Chirag H <chi2019rag@gmail.com>
2025-05-17 21:21:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
-import path from "path";
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), cloudflare()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src/react-app'),
-    },
-  },
-});
+  plugins: [tsconfigPaths()],
+})
